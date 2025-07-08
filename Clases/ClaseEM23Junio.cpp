a9be1bea72b767daa179b75bca31b65347d24fc8
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2025-07-08 00:14:47 -0300

    Taller ICPC - UNLP 2025

--- Clases/ClaseEM23Junio.cpp
+++ Clases/ClaseEM23Junio.cpp
@@ -1,3 +1,34 @@
+/*
+Exponenciacion modular:
+
+Su tarea consiste en calcular eficientemente los valores a^b modulo 10^9 + 7.
+
+Tenga en cuenta que en esta tarea asumimos que 0^0 = 1.
+
+Entrada
+La primera linea de entrada contiene un entero n: el numero de calculos.
+Despues, hay n lineas, cada una con dos enteros a y b.
+
+Salida
+Imprima cada valor a^b modulo 10^9 + 7.
+
+Restricciones
+1 <= n <= 2 * 10^5
+0 <= a,b <= 10^9
+
+Ejemplo
+Entrada:
+3
+3 4
+2 8
+123 123
+
+Salida:
+81
+256
+921450052
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define FIN ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
